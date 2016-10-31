e21f46b8fc3b774699a6b9f64195328d893dac14
Julian NG <juliannieb@gmail.com>
2016-10-30 19:39:10 -0600

    Add signup button functionality in login view

--- app/src/main/java/mx/com/magoo/waterme/LoginActivity.java
+++ app/src/main/java/mx/com/magoo/waterme/LoginActivity.java
@@ -1,13 +1,34 @@
 package mx.com.magoo.waterme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getWidgets();
+        setWidgetsFunctionalities();
+    }
+
+    public void getWidgets() {
+        this.btnSignup = (Button) findViewById(R.id.btnSignup);
+    }
+
+    public void setWidgetsFunctionalities() {
+        this.btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
