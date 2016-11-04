06f8cf3135e718a9763894807ecf2ae4e6ad9016
Julian NG <juliannieb@gmail.com>
2016-11-03 19:18:54 -0600

    Integrate raspberry py with app

--- app/src/main/java/mx/com/magoo/waterme/MyPlantsActivity.java
+++ app/src/main/java/mx/com/magoo/waterme/MyPlantsActivity.java
@@ -1,8 +1,19 @@
 package mx.com.magoo.waterme;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MyPlantsActivity extends AppCompatActivity {
 
@@ -38,5 +49,41 @@ public class MyPlantsActivity extends AppCompatActivity {
         plants[9] = "Plant 10";
         MyPlantsAdapter myPlantsAdapter = new MyPlantsAdapter(MyPlantsActivity.this, R.layout.my_plants_list_item, plants);
         this.listViewMyPlants.setAdapter(myPlantsAdapter);
+        this.listViewMyPlants.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                new WaterTask().execute("http://10.25.49.234/?time=5");
+            }
+        });
+    }
+
+    public class WaterTask extends AsyncTask<String, Void, String> {
+        private static final String TAG = "BackgroundTask";
+
+        @Override
+        protected String doInBackground(String... ulr) {
+            Response response = null;
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    //.header("Authorization", token)
+                    .url(ulr[0])
+                    .build();
+
+            try {
+                response = client.newCall(request).execute();
+                return response.body().string();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            //Log.i("OkHttp :", result);
+            Toast.makeText(MyPlantsActivity.this, result, Toast.LENGTH_LONG).show();
+        }
+
     }
 }
